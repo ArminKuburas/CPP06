508df5558622fc69416ca6844136567601c40ec1
Armin Kuburas <akuburas@c2r1p11.hive.fi>
2024-10-10 15:11:20 +0300

    small changes

--- ex01/Serializer.hpp
+++ ex01/Serializer.hpp
@@ -6,13 +6,14 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 07:57:35 by akuburas          #+#    #+#             */
-/*   Updated: 2024/10/10 08:09:34 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/10/10 14:14:36 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Data.hpp"
+#include <cstdint>
 
 class Serializer
 {
